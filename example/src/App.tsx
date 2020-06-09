425ed6c80a1f57a6cc95a464b3ca59f1732a7cae
LindaLopezR <guadalupelopez.lr@gmail.com>
2020-06-09 11:28:37 -0500

    Archivo de ejemplo

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,17 +2,17 @@ import * as React from 'react';
 import LoginSuite from 'react-native-login-suite';
 
 export default class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     companyShow: false,
-  //     companyState: '',
-  //     usernameState: '',
-  //     passwordState: '',
-  //   };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      companyShow: false,
+      companyState: '',
+      usernameState: '',
+      passwordState: '',
+    };
+  }
   render() {
-    // const { companyShow, companyState, usernameState, passwordState } = this.state;
+    const { companyShow, companyState, usernameState, passwordState } = this.state;
 
     return (
       <LoginSuite
@@ -23,15 +23,15 @@ export default class App extends React.Component {
         holderPasword="Contraseña"
         holderLogin="Entrar"
         version="Versión 1.0"
-        // showCompany={companyShow}
-        // username={usernameState}
-        // password={passwordState}
-        // company={companyState}
-        // toggleCompany={(companyShow) => this.setState({ companyShow })}
-        // changeCompany={(companyState) => this.setState({ companyState })}
-        // changeUsername={(usernameState) => this.setState({ usernameState })}
-        // changePassword={(passwordState) => this.setState({ passwordState })}
-        // handleLogin={() => console.log('Guardar!')}
+        showCompany={companyShow}
+        username={usernameState}
+        password={passwordState}
+        company={companyState}
+        toggleCompany={(companyShow) => this.setState({ companyShow })}
+        changeCompany={(companyState) => this.setState({ companyState })}
+        changeUsername={(usernameState) => this.setState({ usernameState })}
+        changePassword={(passwordState) => this.setState({ passwordState })}
+        handleLogin={() => console.log('Guardar!')}
       />
     );
   }
